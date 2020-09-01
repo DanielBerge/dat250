65c58c1ac97c1d9c8e25748a26a1a11785147a16
Daniel Berge <daniel@berge.us>
2020-09-01 14:48:38 +0200

    Add starter code

---
+++ assignment2/code/src/main/java/no/hvl/dat250/jpa/basicexample/App.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.jpa.basicexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
